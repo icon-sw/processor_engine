2807f73d7c85098f02edefb91765b95b1fd5e7ed
Sofia Silvestri <128178527+icon-sw@users.noreply.github.com>
2025-05-12 22:16:04 +0000

    Implemented UpdInterface

--- src/interfaces.rs
+++ src/interfaces.rs
@@ -1,5 +1,7 @@
 use std::fs::{File, OpenOptions};
 use std::io::{Read, Write};
+use std::net::{UdpSocket, IpAddr, Ipv4Addr, Ipv6Addr};
+use std::str::FromStr;
 use crate::log::{log, LogEntry, LogLevel};
 #[derive(Clone)]
 pub enum PhysInterface {
@@ -15,7 +17,6 @@ pub enum PhysInterface {
 #[derive(Clone)]
 pub enum LogicalInterface {
     File,
-    Log,
     Socket,
     Pipe,
     SharedMemory,
@@ -23,10 +24,11 @@ pub enum LogicalInterface {
     Signal,
 }
 #[derive(Clone)]
-pub enum InterfaceType {
-    Physical(PhysInterface),
-    Logical(LogicalInterface),
+pub struct InterfaceType {
+    phys: PhysInterface,
+    logic: LogicalInterface,
 }
+
 #[derive(Clone)]
 pub enum InterfaceStatus {
     Connected,
@@ -42,9 +44,9 @@ pub enum InterfaceMode {
 }
 #[derive(Clone)]
 pub enum InterfaceProtocol {
-    Custom,
-    TCP,
-    UDP,
+    Raw,
+    TcpIp,
+    UdpIp,
     CANopen,
     EtherCAT,
 }
@@ -61,6 +63,7 @@ pub enum InterfaceError {
     ReadOnWriteOnly,
     NotOpenIFace,
     AlreadyOpenIFace,
+    NotValidSocketAddr,
     GenericError,
 }
 impl ToString for InterfaceError {
@@ -77,6 +80,7 @@ impl ToString for InterfaceError {
             InterfaceError::ReadOnWriteOnly => "Read on Write Only".to_string(),
             InterfaceError::NotOpenIFace => "Interface not open".to_string(),
             InterfaceError::AlreadyOpenIFace => "Interface already open".to_string(),
+            InterfaceError::NotValidSocketAddr => "Not valid socket address".to_string(),
             InterfaceError::GenericError => "Unpredictable error".to_string(),
         }
     }
@@ -96,11 +100,39 @@ struct BaseInterface {
     status: InterfaceStatus,
     mode: InterfaceMode,
     interface_type: InterfaceType,
+    interface_protocol: InterfaceProtocol,
+    log_interface: bool,
     error: Option<InterfaceError>,
     event: Option<InterfaceEvent>,
 }
 
 impl BaseInterface {
+    fn new(
+        name: String,
+        description: String,
+        interface_type: InterfaceType,
+        mode: InterfaceMode,
+        interface_protocol: InterfaceProtocol,
+        log_if: Option<bool>,
+    ) -> Self {
+        BaseInterface {
+            name,
+            description,
+            status: InterfaceStatus::Disconnected,
+            mode,
+            interface_type,
+            interface_protocol,
+            log_interface: {
+                if log_if.is_some() {
+                    log_if.unwrap()
+                } else {
+                    false
+                }
+            },
+            error: None,
+            event: None,
+        }
+    }
     fn get_name(&self) -> String {
         self.name.clone()
     }
@@ -117,7 +149,7 @@ impl BaseInterface {
         self.mode.clone()
     }
     fn get_protocol(&self) -> InterfaceProtocol {
-        InterfaceProtocol::Custom
+        self.interface_protocol.clone()
     }
     fn get_error(&self) -> Option<InterfaceError> {
         self.error.clone()
@@ -126,11 +158,14 @@ impl BaseInterface {
         self.event.clone()
     }
     fn is_log_interface(&self) -> bool {
-        match self.get_type() {
-            InterfaceType::Logical(LogicalInterface::Log) => true,
-            _ => false,
-        }
+        self.log_interface.clone()
     }
+
+    fn set_error(&mut self, error: InterfaceError) {
+        self.error = Some(error);
+        self.log_error();
+    }
+
     fn log_error(&mut self) {
         if self.is_log_interface() {
             return;
@@ -171,22 +206,12 @@ impl FileInterface {
         FileInterface {
             file_path,
             file: None,
-            base_interface: BaseInterface {
-                name,
-                description,
-                status: InterfaceStatus::Disconnected,
-                mode,
-                interface_type: {
-                    if log_if.is_some() && log_if.unwrap() {
-                        InterfaceType::Logical(LogicalInterface::Log)
-                    } else {
-                        InterfaceType::Logical(LogicalInterface::File)
-                    }
-                },
-                error: None,
-                event: None,
-
-            },
+            base_interface: BaseInterface::new(name,
+                                            description,
+                                            InterfaceType{phys: PhysInterface::None, logic: LogicalInterface::File},
+                                            mode,
+                                            InterfaceProtocol::Raw,
+                                            log_if),
         }
     }
 }
@@ -194,7 +219,7 @@ impl InterfaceTrait for FileInterface {
     fn open(&mut self) -> Result<(), String> {
         match  self.base_interface.get_status() {
             InterfaceStatus::Connected => {
-                self.base_interface.error = Some(InterfaceError::AlreadyOpenIFace);
+                self.base_interface.set_error(InterfaceError::AlreadyOpenIFace);
                 return Err(self.base_interface.error.clone().unwrap().to_string());
             }
             _ => {}
@@ -220,7 +245,7 @@ impl InterfaceTrait for FileInterface {
     fn close(&mut self) -> Result<(), String> {
         match self.base_interface.status {
             InterfaceStatus::Disconnected => {
-                self.base_interface.error = Some(InterfaceError::NotOpenIFace);
+                self.base_interface.set_error(InterfaceError::NotOpenIFace);
                 return Err(self.base_interface.error.clone().unwrap().to_string());
             }
             _ => {}
@@ -232,7 +257,7 @@ impl InterfaceTrait for FileInterface {
             self.base_interface.status = InterfaceStatus::Disconnected;
             Ok(())
         } else {
-            self.base_interface.error = Some(InterfaceError::NotOpenIFace);
+            self.base_interface.set_error(InterfaceError::NotOpenIFace);
             return Err(self.base_interface.error.clone().unwrap().to_string());
         }
     }
@@ -240,7 +265,7 @@ impl InterfaceTrait for FileInterface {
     fn read(&mut self, buffer: &mut [u8]) -> Result<u32, String> {
         match self.base_interface.mode {
             InterfaceMode::Write => {
-                self.base_interface.error = Some(InterfaceError::WriteOnReadOnly);
+                self.base_interface.set_error(InterfaceError::WriteOnReadOnly);
                 return Err(self.base_interface.error.clone().unwrap().to_string());
             }
             _ => {}
@@ -252,12 +277,12 @@ impl InterfaceTrait for FileInterface {
                     let bytes_read = file.read(buffer).map_err(|e| e.to_string())?;
                     return Ok(bytes_read as u32);
                 } else {
-                    self.base_interface.error = Some(InterfaceError::GenericError);
+                    self.base_interface.set_error(InterfaceError::GenericError);
                     return Err(self.base_interface.error.clone().unwrap().to_string());
                 }
             }
             _ => {
-                self.base_interface.error = Some(InterfaceError::NotOpenIFace);
+                self.base_interface.set_error(InterfaceError::NotOpenIFace);
                 return Err(self.base_interface.error.clone().unwrap().to_string());
             }
         }
@@ -266,7 +291,7 @@ impl InterfaceTrait for FileInterface {
     fn write(&mut self, buffer: &[u8]) -> Result<(), String> {
         match self.base_interface.mode {
             InterfaceMode::Read => {
-                self.base_interface.error = Some(InterfaceError::ReadOnWriteOnly);
+                self.base_interface.set_error(InterfaceError::ReadOnWriteOnly);
                 return Err(self.base_interface.error.clone().unwrap().to_string());
             }
             _ => {}
@@ -279,15 +304,185 @@ impl InterfaceTrait for FileInterface {
                     return Ok(());
                 }
                 else {
-                    self.base_interface.error = Some(InterfaceError::GenericError);
+                    self.base_interface.set_error(InterfaceError::GenericError);
                     return Err(self.base_interface.error.clone().unwrap().to_string());
                 }
             }
             _ => {
-                self.base_interface.error = Some(InterfaceError::NotOpenIFace);
+                self.base_interface.set_error(InterfaceError::NotOpenIFace);
                 return Err(self.base_interface.error.clone().unwrap().to_string());
             }
         }
     }
 }
 
+pub struct UDPInterface {
+    ip_address: String,
+    port: u16,
+    socket: Option<UdpSocket>,
+    remote_addr: String,
+    remote_port: u16,
+    remote_socket_addr: Option<std::net::SocketAddr>,
+    multicast: bool,
+    base_interface: BaseInterface,
+}
+impl UDPInterface {
+    pub fn new(name: String, description: String, ip_address: String, port: u16, log_if: Option<bool>) -> Self {
+        if format!("{}:{}", ip_address, port).parse::<std::net::SocketAddr>().is_err() {
+            panic!("Invalid IP address or port");
+        }
+        UDPInterface {
+            ip_address,
+            port,
+            remote_addr: "".to_string(),
+            remote_port: 0,
+            socket: None,
+            remote_socket_addr: None,
+            multicast: false,
+            base_interface: BaseInterface::new(name,
+                                            description,
+                                            InterfaceType{phys: PhysInterface::Ethernet, logic: LogicalInterface::Socket},
+                                            InterfaceMode::ReadWrite,
+                                            InterfaceProtocol::UdpIp,
+                                            log_if),
+        }
+    }
+    pub fn append_remote_addr(&mut self, remote_ip: String, remote_port: u16) {
+        match self.base_interface.get_mode() {
+            InterfaceMode::Read => {
+                self.base_interface.set_error(InterfaceError::ReadOnWriteOnly);
+                return;
+            }
+            _ => {}
+        }
+        let socket_addr = format!("{}:{}", remote_ip, remote_port);
+        if socket_addr.parse::<std::net::SocketAddr>().is_err() {
+            self.base_interface.set_error(InterfaceError::NotValidSocketAddr);
+            return;
+        }
+
+        self.remote_addr = remote_ip;
+        self.remote_port = remote_port;
+    }
+}
+
+impl InterfaceTrait for UDPInterface {
+    fn open(&mut self) -> Result<(), String> {
+        match  self.base_interface.get_status() {
+            InterfaceStatus::Connected => {
+                self.base_interface.set_error(InterfaceError::AlreadyOpenIFace);
+                return Err(self.base_interface.error.clone().unwrap().to_string());
+            }
+            _ => {}
+        }
+        // Implement UDP connection opening logic here
+        self.socket = Some(UdpSocket::bind((self.ip_address.as_str(), self.port)).map_err(|e| e.to_string())?);
+        let remote_ip_addr = self.remote_addr.as_str();
+        match IpAddr::from_str(remote_ip_addr) {
+            Ok(ip_addr) => {
+                if ip_addr.is_multicast() {
+                    let socket = self.socket.as_ref().unwrap();
+                    if ip_addr.is_ipv4() {
+                        let ipv4 = Ipv4Addr::from_str(remote_ip_addr).map_err(|e| e.to_string())?;
+                        socket.set_multicast_loop_v4(true).map_err(|e| e.to_string())?;
+                        socket.join_multicast_v4(&ipv4, &Ipv4Addr::new(0, 0, 0, 0))
+                                .map_err(|e| e.to_string())?;
+                    }
+                    else {
+                        let ipv6 = Ipv6Addr::from_str(remote_ip_addr).map_err(|e| e.to_string())?;
+                        socket.set_multicast_loop_v6(true).map_err(|e| e.to_string())?;
+                        socket.join_multicast_v6(&ipv6, 0).map_err(|e| e.to_string())?;
+                    }
+                }
+                self.remote_socket_addr = Some(format!("{}:{}", remote_ip_addr, self.remote_port)
+                                            .parse::<std::net::SocketAddr>().map_err(|e| e.to_string())?);
+            }
+            Err(_) => {}
+        }
+
+        self.base_interface.status = InterfaceStatus::Connected;
+        Ok(())
+    }
+
+    fn close(&mut self) -> Result<(), String> {
+        match self.base_interface.status {
+            InterfaceStatus::Disconnected => {
+                self.base_interface.set_error(InterfaceError::NotOpenIFace);
+                return Err(self.base_interface.error.clone().unwrap().to_string());
+            }
+            _ => {}
+        }
+        // Implement UDP connection closing logic here
+        if let Some(ref socket) = self.socket {
+            self.base_interface.status = InterfaceStatus::Disconnected;
+            if self.multicast {
+                if let Some(ref remote_addr) = self.remote_socket_addr {
+                    if remote_addr.is_ipv6() {
+                        socket.leave_multicast_v6(&Ipv6Addr::from_str(&self.remote_addr).unwrap(), 0)
+                            .map_err(|e| e.to_string())?;
+                    }
+                    else {
+                        socket.leave_multicast_v4(&Ipv4Addr::from_str(&self.remote_addr).unwrap(), &Ipv4Addr::new(0, 0, 0, 0))
+                            .map_err(|e| e.to_string())?;
+                    }
+                }
+            }
+            Ok(())
+        }
+        else {
+            self.base_interface.set_error(InterfaceError::NotOpenIFace);
+            return Err(self.base_interface.error.clone().unwrap().to_string());
+        }
+
+    }
+
+    fn read(&mut self, buffer: &mut [u8]) -> Result<u32, String> {
+        // Implement UDP reading logic here
+        match self.base_interface.get_mode() {
+            InterfaceMode::Write => {
+                self.base_interface.set_error(InterfaceError::WriteOnReadOnly);
+                return Err(self.base_interface.error.clone().unwrap().to_string());
+            }
+            _ => {}
+        }
+        if self.socket.is_some() {
+            if let Some(ref socket) = self.socket {
+                let (bytes_read, _) = socket.recv_from(buffer).map_err(|e| e.to_string())?;
+                return Ok(bytes_read as u32);
+            }
+            else {
+                self.base_interface.set_error(InterfaceError::GenericError);
+                return Err(self.base_interface.error.clone().unwrap().to_string());
+            }
+        }
+        else {
+            self.base_interface.set_error(InterfaceError::NotOpenIFace);
+            return Err(self.base_interface.error.clone().unwrap().to_string());
+        }
+    }
+
+    fn write(&mut self, buffer: &[u8]) -> Result<(), String> {
+        // Implement UDP writing logic here
+        match self.base_interface.get_mode() {
+            InterfaceMode::Read => {
+                self.base_interface.set_error(InterfaceError::ReadOnWriteOnly);
+                return Err(self.base_interface.error.clone().unwrap().to_string());
+            }
+            _ => {}
+        }
+        if let Some(ref remote_addr) = self.remote_socket_addr {
+            if let Some(ref socket) = self.socket {
+                socket.send_to(buffer, remote_addr).map_err(|e| e.to_string())?;
+                return Ok(());
+            }
+            else {
+                self.base_interface.set_error(InterfaceError::NotOpenIFace);
+                return Err(self.base_interface.error.clone().unwrap().to_string());
+            }
+        }
+        else {
+            self.base_interface.set_error(InterfaceError::GenericError);
+            return Err(self.base_interface.error.clone().unwrap().to_string());
+        }
+    }
+}
